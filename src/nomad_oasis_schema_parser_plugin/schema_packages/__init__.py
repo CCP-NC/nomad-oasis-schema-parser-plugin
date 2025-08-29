d4e0eaf775ef1062a97ae01d7c26cfba56ccaf4c
Sathya-S3 <s3.at.work@gmail.com>
2025-08-29 12:44:42 +0000

    ccpnc schema entry point

--- src/nomad_oasis_schema_parser_plugin/schema_packages/__init__.py
+++ src/nomad_oasis_schema_parser_plugin/schema_packages/__init__.py
@@ -2,16 +2,18 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class CCPNCSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_oasis_schema_parser_plugin.schema_packages.schema_package import m_package
+        from nomad_oasis_schema_parser_plugin.schema_packages.schema_package import (
+            m_package,
+        )
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+ccpnc_schema_entry_point = CCPNCSchemaPackageEntryPoint(
+    name='CCPNCSchemaPackage',
+    description='CCPNC schema package entry point configuration.',
 )
